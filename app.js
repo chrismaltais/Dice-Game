ab5028b1b65ca2ca5119fe64e23ce450cd98ecb6
Chris Maltais <chris.maltais@hotmail.ca>
2017-01-10 21:33:38 -0500

    Changed player names to be more general

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ function init () {
     document.getElementById('score-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
-    document.getElementById('name-1').textContent = 'Chris';
-    document.getElementById('name-0').textContent = 'JP';
+    document.getElementById('name-1').textContent = 'Player 1';
+    document.getElementById('name-0').textContent = 'Player 2';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
